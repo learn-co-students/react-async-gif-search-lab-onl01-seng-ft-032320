c004686ecab56c191c6e816e129f8dcbcea86287
antdp425 <antdp425@aol.com>
2020-07-26 10:42:13 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,18 @@
-import React from 'react'
-
-import NavBar from './NavBar'
-
+// https://api.giphy.com/v1/gifs/search?q=[QUERY]&api_key=Ie8Qm2GmVgFkaHA6uQuvi1LlHkQ1Yujp&rating=g
 // the App component should render out the GifListContainer component 
 
-const App = () => {
-  return (
-    <div>
-        < NavBar color='black' title="Giphy Search" />
-    </div>
-  )
-}
+import React, { Component } from 'react'
+import NavBar from './NavBar'
+import GifListContainer from '../containers/GifListContainer' 
 
-export default App
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        < NavBar color='black' title="Giphy Search" />
+        < GifListContainer />
+      </div>
+    )
+  }
+}
---
+++ src/containers/GifListContainer.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import GifList from '../components/GifList'
+import GifSearch from '../components/GifSearch' 
+
+export default class GifListContainer extends Component {
+
+   state = {
+      gifs: []
+   }
+
+   componentDidMount(){
+      this.getGifs()
+   }
+
+   getGifs = (query = "spongebob") => {
+
+      fetch(`https://api.giphy.com/v1/gifs/search?q=${query}&api_key=Ie8Qm2GmVgFkaHA6uQuvi1LlHkQ1Yujp&rating=g&limit=3`)
+      .then(resp => resp.json())
+      .then(data => this.setState({
+         gifs: data.data
+      }))
+
+   }
+
+   render() {
+      let i = 0
+      let gifs = this.state.gifs.map(gif => <GifList key={i += 1} gif={gif}/>)
+      return (
+         <div>
+            < GifSearch handleSubmit={this.getGifs}/>
+            <ul>
+               {gifs}
+            </ul>
+         </div>
+      )
+   }
+}
